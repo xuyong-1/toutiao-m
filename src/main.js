f20025bcc6e5ded942a4cbc24c5c4b17a5e7e84c
xuyong <734195222@qq.com>
2022-04-23 18:24:49 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// 加载全局样式
 import './styles/index.less'
 
+// 加载 Vant 全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 动态设置 REM 基准值
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
+
+// 注册使用 Vant 组件库
+Vue.use(Vant)
 
 new Vue({
   router,
